90be8618f256d2680a560b79e4196c008061996d
Aaronmacing <676071068@qq.com>
2015-11-16 14:55:17 +0800

    The pie chart
    用来画饼状图的demo

---
+++ LXMPieViewDemo/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  TEST_Pie
+//
+//  Created by luxiaoming on 15/10/14.
+//  Copyright © 2015年 luxiaoming. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LXMPieView.h"
+
+@interface ViewController ()<LXMPieViewDelegate>
+
+@property (nonatomic, strong) LXMPieView *pieView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *modelArray = [NSMutableArray array];
+    NSArray *valueArray = @[@(10), @(20), @(30), @(40)];
+    NSArray *colorArray = @[[UIColor redColor],
+                            [UIColor orangeColor],
+                            [UIColor yellowColor],
+                            [UIColor greenColor],];
+    for (int i = 0 ; i <valueArray.count ; i++) {
+        LXMPieModel *model = [[LXMPieModel alloc] initWithColor:colorArray[i] value:[valueArray[i] floatValue] text:nil];
+        [modelArray addObject:model];
+    }
+    
+    LXMPieView *pieView = [[LXMPieView alloc] initWithFrame:CGRectMake(50, 50, 250, 250) values:modelArray];
+    pieView.delegate = self;
+    [self.view addSubview:pieView];
+    self.pieView = pieView;
+    
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    [button setTitle:@"play" forState:UIControlStateNormal];
+    button.center = self.view.center;
+    button.backgroundColor = [UIColor purpleColor];
+    [button addTarget:self action:@selector(handleButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - LXMPieViewDelegate
+
+- (void)lxmPieView:(LXMPieView *)pieView didSelectSectionAtIndex:(NSInteger)index {
+    NSLog(@"didSelectSectionAtIndex : %@", @(index));
+}
+
+#pragma mark - buttonAction
+
+- (void)handleButtonTapped:(UIButton *)sender {
+    [self.pieView reloadData];
+}
+
+@end
